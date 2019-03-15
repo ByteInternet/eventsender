ada7bf370665129fb38ad4798162b99dda877a35
Bono de Visser <bono@byte.nl>
2019-03-15 14:54:19 +0100

    Improve setup.py
    Provide long description for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+
+def readfile(filename):
+    path = join(dirname(abspath(__file__)), filename)
+    with open(path, 'rt') as filehandle:
+        return filehandle.read()
+
 
 setup(
     name='eventsender',
-    version='1.1',
+    version='1.1.1',
     packages=find_packages(exclude=['tests*']),
     url='https://github.com/ByteInternet/eventsender',
     author='Byte B.V.',
@@ -21,5 +30,7 @@ setup(
     ],
     keywords='amqp rabbitmq events eventsender',
     description='RabbitMQ event sender',
+    long_description=readfile('README.rst'),
+    long_description_content_type='text/x-rst',
     install_requires=['pika==0.13.0'],
 )
